33e9e8936fcff28d3cd736790fbe4f15eb5f5e5f
liuweiyi <2284069530@qq.com>
2022-03-25 17:00:13 +0800

    增加聚合函数select

--- src/main/java/com/lwy/demo/controller/RegisterController.java
+++ src/main/java/com/lwy/demo/controller/RegisterController.java
@@ -25,11 +25,14 @@ public class RegisterController {
     public ResultDTO register(@RequestBody User user) throws Exception {
         ResultDTO register = registerService.register(user);
         if(register.getType() == true){
+            //默认 user表
             sqlService.sqlUnSelect(user,"create table user(id int(10) primary key auto_increment,name varchar(10),age int(10));");
             sqlService.sqlUnSelect(user,"insert into user (name,age) values ('张三',18)");
             sqlService.sqlUnSelect(user,"insert into user (name,age) values ('李四',19)");
             sqlService.sqlUnSelect(user,"insert into user (name,age) values ('成龙',50)");
             sqlService.sqlUnSelect(user,"insert into user (name,age) values ('马云',51)");
+            //sql查询记录表
+            sqlService.sqlUnSelect(user,"create table sqlRecord(id int(10) primary key auto_increment,sqlstatement varchar(255),time varchar(20),type varchar(20),result varchar(20),errorlog varchar(255));");
         }
 
         return register;
--- src/main/java/com/lwy/demo/controller/SqlController.java
+++ src/main/java/com/lwy/demo/controller/SqlController.java
@@ -1,6 +1,7 @@
 package com.lwy.demo.controller;
 
 import com.lwy.demo.TO.ResultDTO;
+import com.lwy.demo.entity.SqlResult;
 import com.lwy.demo.entity.User;
 import com.lwy.demo.service.SqlService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +20,15 @@ public class SqlController {
 
     @PostMapping("/executeSql")
     public ResultDTO executeSql(@RequestParam String sql,
-                                @RequestParam String token) throws Exception {
+                                @RequestParam String token,
+                                @RequestParam(defaultValue = "0") int isMySql) throws Exception {
         ResultDTO resultDTO = new ResultDTO();
+        sql = sql.trim();
+        //sql执行日志
+        SqlResult sqlResult = new SqlResult();
+        sqlResult.setSqlstatement(sql);
+        sqlResult.setTime(Long.toString(System.currentTimeMillis()));
+
         //判断为空
         if (StringUtils.isEmpty(sql) || StringUtils.isEmpty(token)) {
             resultDTO.setType(false);
@@ -41,6 +49,39 @@ public class SqlController {
         } else {
             resultDTO = sqlService.sqlUnSelect(user, sql);
         }
+        if("sql success".equals(resultDTO.getObject())){
+            sqlResult.setResult("success");
+        }
+        else{
+            sqlResult.setResult("error");
+
+        }
+        //解析sql类型
+        String[] sqlArr = sql.trim().split(" ");
+        String firstSql = sqlArr[0];
+        if(firstSql.equals("select")){
+            sqlResult.setType("select");
+        }
+        else if(firstSql.equals("insert")){
+            sqlResult.setType("insert");
+        }
+        else if(firstSql.equals("update")){
+            sqlResult.setType("update");
+        }
+        else if(firstSql.equals("delete")){
+            sqlResult.setType("delete");
+        }
+        else {
+            sqlResult.setType("other");
+        }
+        //插入sqlresult
+        String sqlResultSql = "insert into sqlrecord(sqlstatement,time,type,result) values( ' ";
+        sqlResultSql += sqlResult.getSqlstatement() + "','" + sqlResult.getTime() + "','" + sqlResult.getType() + "','" + sqlResult.getResult() +"')";
+        //不是sql输入框执行的sql不添加进日志
+        if(isMySql == 1){
+            sqlService.sqlUnSelect(user,sqlResultSql);
+        }
+        System.out.println(sqlResultSql);
         return resultDTO;
     }
 
